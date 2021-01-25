5899bd278584f7331d53f9bcf18590de0efe2564
Anton Milasenko <48668353+Miant1@users.noreply.github.com>
2021-01-25 09:08:40 +0200

    last changes from 25.01

--- assets/js/adv_Shares.js
+++ assets/js/adv_Shares.js
@@ -13,8 +13,35 @@ $(document).ready(function(){
     });
 //конец табы/**
 
+// Charts
 
+/* var popCanvas = $("#popChart");
+var popCanvas = document.getElementById("popChart");
+var popCanvas = document.getElementById("popChart").getContext("2d");
 
+var barChart = new Chart(popCanvas, {
+    type: 'line',
+    data: {
+      labels: ["India", "United States", "Indonesia", "Brazil", "Pakistan", "Nigeria", "Bangladesh", "Russia", "Japan", "China"],
+      datasets: [{
+        label: 'Population',
+        data: [1281935911, 326625791, 260580739, 207353391, 204924861, 190632261, 157826578, 142257519, 126451398, 1379302771],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.4)',
+          'rgba(54, 162, 235, 0.6)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 0.6)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 0.6)',
+          'rgba(255, 99, 132, 0.6)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 0.6)',
+          'rgba(75, 192, 192, 0.6)',
+          'rgba(153, 102, 255, 0.6)'
+        ]
+      }]
+    }
+  }); */
 
 
 });/**
